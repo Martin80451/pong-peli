640f531fe1f38f2052e7ad7711db3156a2a2b6bc
Martin80451 <martin.malmivirtahky@gmail.com>
2024-11-24 14:19:00 +0200

    - Added collision detection for ball + player and AI bars

--- ball.gd
+++ ball.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var areaEntered = 0
 var speed = 120
 func _ready():
 	velocity = Vector2(-200, 200).normalized() * speed
@@ -8,3 +9,9 @@ func _process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
+
+
+
+func _on_collision_detector_area_entered(area: Area2D):
+	areaEntered += 1
+	print(areaEntered)
--- main.gd
+++ main.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_start_game_button_pressed():
-	get_tree().change_scene_to_file("res://Peli näkymä.tscn")
+	get_tree().change_scene_to_file("res://game_view.tscn")
