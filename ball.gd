0b24bfe98a9d91c798d5d371b568d9c7d597d349
Martin80451 <martin.malmivirtahky@gmail.com>
2024-11-24 13:33:29 +0200

    - Added ball movement

--- ball.gd
+++ ball.gd
@@ -1,13 +1,10 @@
 extends CharacterBody2D
 
-var speed = 50
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+var speed = 120
+func _ready():
 	velocity = Vector2(-200, 200).normalized() * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity * velocity.bounce(collision.get_normal())
+		velocity = velocity.bounce(collision.get_normal())
--- player.gd
+++ player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 var speed = 10
 
-func _physics_process(delta):
-	var velocity = Vector2.ZERO
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -=1
 	if Input.is_action_pressed("ui_down"):
