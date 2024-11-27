70a13d341c8b5bbab0b4fef32acd2b90c964a58d
Martin80451 <martin.malmivirtahky@gmail.com>
2024-11-27 14:20:11 +0200

    - Added a working score system. - Added score labels for player and ai.

--- ball.gd
+++ ball.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var area_entered = 0
 var initial_pos
+var ai_score = 0
+var player_score = 0
 var speed = 300
 @onready var label = get_node("/root/Pong/CountdownLabel")
 @onready var timer = get_node("/root/Pong/Timer")
@@ -13,10 +15,10 @@ func _ready():
 func _process(delta):
 	#print(speed)
 	var collision = move_and_collide(velocity * delta)
-	if position.x > 1154 or position.x < 0:
-		position = initial_pos
-		label.visible = true
-		stop_ball()
+	if position.x > 1154:
+		handle_ball("player")
+	elif position.x < 0:
+		handle_ball("ai")
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
 
@@ -27,8 +29,17 @@ func _on_collision_detector_area_entered(area: Area2D):
 func stop_ball():
 	velocity = Vector2.ZERO
 	timer.start()
+	
+func handle_ball(type):
+	match type:
+		"player":
+			player_score += 1
+		"ai":
+			ai_score += 1
+	position = initial_pos
+	label.visible = true
+	stop_ball()
 
 func _on_timer_timeout():
-	print("test")
 	label.visible = false
 	velocity = Vector2(-200, 200).normalized() * speed
--- game_view.gd
+++ game_view.gd
@@ -4,4 +4,6 @@ func _ready():
 	pass 
 
 func _process(delta):
+	$PlayerScoreLabel.text = str($Ball.player_score)
+	$AIScoreLabel.text = str($Ball.ai_score)
 	$CountdownLabel.text = str(int($Timer.time_left) + 1)
