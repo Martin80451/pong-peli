b819da8bdce3cb15474b27535646e35d4ed75659
Martin80451 <martin.malmivirtahky@gmail.com>
2024-12-01 13:03:44 +0200

    - Added logic for the game to end.

--- game_view.gd
+++ game_view.gd
@@ -7,3 +7,7 @@ func _process(delta):
 	$PlayerScoreLabel.text = str($Ball.player_score)
 	$AIScoreLabel.text = str($Ball.ai_score)
 	$CountdownLabel.text = str(int($Timer.time_left) + 1)
+	if $Ball.player_score == 7:
+		get_tree().change_scene_to_file("res://game_won.tscn")
+	elif $Ball.ai_score == 7:
+		get_tree().change_scene_to_file("res://game_lost.tscn")
